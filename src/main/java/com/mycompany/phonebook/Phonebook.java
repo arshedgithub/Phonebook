e03868711f4fc062260b1953cb10917147715fd0
arshedgithub <arshedahmedmail@gmail.com>
2021-06-16 22:36:19 +0530

    add buttons & labels

--- src/main/java/com/mycompany/phonebook/Phonebook.java
+++ src/main/java/com/mycompany/phonebook/Phonebook.java
@@ -1,11 +1,16 @@
 
 package com.mycompany.phonebook;
 
+import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JTable;
@@ -21,67 +26,90 @@ import java.util.Vector;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 /**
  * @author Arshed
  */
 public class Phonebook extends JFrame{
 
-    JPanel panel1;
-    
-    JTextField txtName;
-    JTextField txtTelephone;
-    JTextField txtEmail;
-    JTextField txtSearchByName;
+    JTextField txtName = new JTextField(30), txtPhone = new JTextField(15), txtEmail = new JTextField(30), txtSearchName = new JTextField(20);
     
     JTable tblStudent;
 
-    JComboBox<String> cmbType;
-    JComboBox<String> cmbSearchByType;
-
+    JComboBox<String> cmbType = new JComboBox<>();
+    JComboBox<String> cmbSearchByType = new JComboBox<>();
+    JButton addBtn = new JButton("Add"), updateBtn = new JButton("Update"), clearBtn = new JButton("Clear"), deleteBtn = new JButton("Delete");
+    JButton search = new JButton("Search"), clearSearch = new JButton("Clear");
+    JPanel pane1, pane2, pane3;
+    
+    Font font = new Font("Serif",Font.PLAIN,30);
+    
     Phonebook(){
         
         setTitle("Phone Book");
         setLocation(300,200);
-        setSize(600,400);
+        setSize(500,500);
 
         Container con = getContentPane();
-        FlowLayout lay = new FlowLayout();
-        con.setLayout(lay);
+        con.setFont(font);
+        con.setLayout(new FlowLayout());
+        
+        pane1 = new JPanel();
+        pane1.setBounds(10, 10, 480, 150); 
+        pane1.setBackground(Color.red);
+        pane1.setLayout(new GridBagLayout());
 
-        panel1 = new JPanel();
-        JLabel lblName = new JLabel("Name :");
-        JLabel lblTelephone = new JLabel("Telephone :");
-        JLabel lblEmail = new JLabel("Email          :");
-        JLabel lblType = new JLabel("Type            :");
+        addItem(pane1, new JLabel("Name :"), 0, 0, 1, 1, GridBagConstraints.WEST);
+        addItem(pane1, new JLabel("Phone :"), 0, 1, 1, 1, GridBagConstraints.WEST);
+        addItem(pane1, new JLabel("Address :"), 0, 2, 1, 1, GridBagConstraints.WEST);
+        addItem(pane1, new JLabel("Type :"), 0, 3, 1, 1, GridBagConstraints.WEST);
+ 
+        String typeData[] = { "Family", "Friend ", "Relation", "Other  " };
+        DefaultComboBoxModel typeModel = new DefaultComboBoxModel(typeData);
+        cmbType.setModel(typeModel);
 
-        txtName = new JTextField(45);
-        txtTelephone = new JTextField(45);
-        txtEmail = new JTextField(45);
+        addItem(pane1, txtName, 1, 0, 2, 1, GridBagConstraints.WEST);
+        addItem(pane1, txtPhone, 1, 1, 1, 1, GridBagConstraints.WEST);
+        addItem(pane1, txtEmail, 1, 2, 2, 1, GridBagConstraints.WEST);
+        addItem(pane1, cmbType, 1, 3, 1, 1, GridBagConstraints.WEST);
+        
+        pane2 = new JPanel();
+        pane2.setBounds(40,170,500,50);
+        pane2.setBackground(Color.BLUE);
+        pane2.setLayout(new FlowLayout());
+        pane2.add(addBtn);
+        pane2.add(updateBtn);
+        pane2.add(clearBtn);
+        pane2.add(deleteBtn);
+        
+        pane3 = new JPanel();
+        pane3.setBounds(40, 250, 450, 100);
+        pane3.setBackground(Color.GREEN);
+        pane3.setLayout(new GridBagLayout());
 
-//        String typeData[] = { "Family", "Friend ", "Relation", "Other  " };
-//        DefaultComboBoxModel typeModel = new DefaultComboBoxModel(typeData);
-//        cmbType = new JComboBox<>();
-//        cmbType.setModel(typeModel);
-//
-//        JButton btnAdd = new JButton("       Add       ");
-//        JButton btnClear = new JButton("      Clear      ");  
-//        JButton btnUpdate = new JButton("     Update     ");
-//        JButton btnDelete = new JButton("     Delete      ");
-//      
-//        JLabel lblSearchByName = new JLabel("Search By Name                    :");
-//        JLabel lblSearchByType = new JLabel("Search By Type                      :");
-//
-//        txtSearchByName = new JTextField(37);
-//   
-//        String sbtypeData[] = { "All                                                                                                                           " };
-//        DefaultComboBoxModel sbtypeModel = new DefaultComboBoxModel(sbtypeData);
-//        cmbSearchByType = new JComboBox<>();
-//        cmbSearchByType.setModel(sbtypeModel);
-//
-//        JButton btnSearch = new JButton("        Search         ");
-//        JButton btnSearchClear = new JButton("     Search Clear     ");
+        Box searchBox = Box.createVerticalBox();
+        searchBox.setBorder(BorderFactory.createTitledBorder("search"));
+        con.add(searchBox);
+        
+        String searchTypeData[] = { "All", "Family", "Friend ", "Relation", "Other" };
+        DefaultComboBoxModel searchTypeModel = new DefaultComboBoxModel(searchTypeData);
+        cmbSearchByType.setModel(searchTypeModel);
+
+        addItem(pane3, new JLabel("Name :"), 0, 0, 1, 1, GridBagConstraints.WEST);
+        addItem(pane3, txtSearchName, 1, 0, 1, 1, GridBagConstraints.WEST);
+        addItem(pane3, new JLabel("Type :"), 2, 0, 1, 1, GridBagConstraints.WEST);
+        addItem(pane3, cmbSearchByType, 3, 0, 1, 1, GridBagConstraints.WEST);
+        addItem(pane3, search, 0, 1, 1, 1, GridBagConstraints.EAST);
+        addItem(pane3, clearSearch, 1, 1, 1, 1, GridBagConstraints.WEST);
+
+
+
+        
 //
 //        btnAdd.addActionListener( new ActionListener() { public void actionPerformed(ActionEvent e){ btnAddAP(e);  }    }   );
 //    
@@ -128,13 +156,18 @@ public class Phonebook extends JFrame{
 //
 //        jspStTable.setViewportView( tblStudent );
 //
-        con.add(lblName);
-        con.add(txtName);
-        con.add(lblTelephone);
-        con.add(txtTelephone);
-        con.add(lblEmail);
-        con.add(txtEmail);
-        con.add(lblType);
+        con.add(pane1);
+        con.add(pane2);
+        con.add(pane3);
+
+
+//        con.add(lblName);
+//        con.add(txtName);
+//        con.add(lblTelephone);
+//        con.add(txtTelephone);
+//        con.add(lblEmail);
+//        con.add(txtEmail);
+//        con.add(lblType);
 //        con.add(cmbType);
 //        con.add(btnAdd);
 //        con.add(btnClear);
@@ -148,14 +181,15 @@ public class Phonebook extends JFrame{
 //        con.add(btnSearchClear);
 //
 //        con.add(jspStTable);
-
+            pane1.setVisible(true);
 
     }
 
     public void btnAddAP(ActionEvent e){
+        
         String name = txtName.getText();
         String type = cmbType.getSelectedItem().toString();
-        String telephone = txtTelephone.getText();
+        String telephone = txtPhone.getText();
         String email = txtEmail.getText();
 
         Vector r = new Vector();
@@ -166,7 +200,20 @@ public class Phonebook extends JFrame{
 
         DefaultTableModel model = (DefaultTableModel)tblStudent.getModel();
         model.addRow(r);
+        
     }
 
-
+    private void addItem(JPanel p, JComponent c, int x,int y, int width, int height, int align){
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.gridx = x;
+        gc.gridy = y;
+        gc.gridheight = height;
+        gc.gridwidth = width;
+        gc.weightx = 100.0;
+        gc.weighty = 100.0;
+        gc.insets = new Insets(5, 5, 5, 5);
+        gc.anchor = align;
+        gc.fill = GridBagConstraints.NONE;
+        p.add(c,gc);
+    }
 }
